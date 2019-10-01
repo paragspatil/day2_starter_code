c4b226b9d2fb6087d821bdc94d18f7d4baa16ca2
Parag <paragspatil123@gmail.com>
2019-10-01 10:54:45 +0530

    Day 2 startup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
+  Container container1 = Container(
+
+    height: 100,
+    color: Colors.orange,
+    margin: EdgeInsets.only(bottom: 20),
+
+
+  );
+  Container container2 = Container(
+    color: Colors.white,
+    height: 100,
+
+      margin: EdgeInsets.only(bottom: 20),
+    child: Image(
+      image: NetworkImage('https://raw.githubusercontent.com/McLarenCollege/Flutter-Course-Notes/master/ensign.png')
+
+    ),
+
+  );
+
+
+
+
+
+  Container container3 = Container(
+
+    height: 100,
+    color: Colors.green,
+
+
+
+  );
+  List<Widget> containers = [container1, container2, container3];
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Container(),
-      ),
+          backgroundColor: Colors.black,
+          body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: containers,
     ),
+  ),
+  ),
+  ),
   );
 }
